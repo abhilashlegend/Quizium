b7d326edf9c632c5b819cbd738b43b63745dbb58
abhilashlegend <abhilash456a@live.com>
2026-03-06 22:08:10 +0530

    Configured file upload on server

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,35 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
 const authRoutes = require('./routes/auth');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuidv4() + '-' + file.originalname)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if( file.mimetype === 'image/png'  ||
+        file.mimetype === 'image/jpg'  ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
 app.use(bodyParser.json());
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
