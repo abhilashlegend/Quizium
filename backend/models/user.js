1561739e69e8681771819c2839d41892f2d9aa59
abhilashlegend <abhilash456a@live.com>
2026-03-06 21:31:34 +0530

    Added home page

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String },
-    username: { type: String, required: true, unique: true },
     picture: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
